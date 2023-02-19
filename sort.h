aac7a6ef70add71cd5186bd5a293db1edf7259f0
GMDLULI <gugulethumdluli98@gmail.com>
2023-02-19 23:54:17 +0000

    sorting algorithms

--- sort.h
+++ sort.h
@@ -18,15 +18,20 @@ typedef struct listint_s
             struct listint_s *next;
 } listint_t;
 
-void swap(int *a, int *b);
-void swap2(int *array, size_t size, int *a, int *b);
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
-int lomuto(int *array, int low, int high, size_t size);
-void quicksort_recursion(int *array, int low, int high, size_t size);
 
+void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+void counting_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
+void heap_sort(int *array, size_t size);
+void radix_sort(int *array, size_t size);
+void bitonic_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
+void sort_deck(deck_node_t **deck);
 #endif /* SORT_H */
