d3c51659a0e3f9f6eb13f8d459043716136cc9c3
KingHorey <oreoluwaakinbo.oa@gmail.com>
2023-02-19 21:31:53 +0100

    renamed test

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -16,7 +16,7 @@ void bubble_sort(int *array, size_t size)
 	{
 		for (i = 0; i < size; i++)
 		{
-			for (j = 0; j < size - 1; j++)
+			for (j = 0; j < size - 1 - i; j++)
 			{
 				temp = 0;
 				if (array[j] > array[j + 1])
