7a7b01b95ac02a01d080c50b9d8c888bb88b7ab8
KingHorey <oreoluwaakinbo.oa@gmail.com>
2023-02-19 21:22:50 +0100

    new test

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -8,22 +8,24 @@
  *
  */
 
-
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, j, temp;
 
-	for (i = 0; i < size; i++)
+	if (size > 2)
 	{
-		for (j = 0; j < size - 1; j++)
+		for (i = 0; i < size; i++)
 		{
-			temp = 0;
-			if (array[j] > array[j + 1])
+			for (j = 0; j < size - 1; j++)
 			{
-				temp = array[j + 1];
-				array[j + 1] = array[j];
-				array[j] = temp;
-				print_array(array, size);
+				temp = 0;
+				if (array[j] > array[j + 1])
+				{
+					temp = array[j + 1];
+					array[j + 1] = array[j];
+					array[j] = temp;
+					print_array(array, size);
+				}
 			}
 		}
 	}
