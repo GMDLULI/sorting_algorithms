4a4e5b6c193701736956fff7873d1fb3e304d744
GMDLULI <gugulethumdluli98@gmail.com>
2023-02-20 07:19:04 +0000

    selection

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -25,19 +25,18 @@ void bubble_sort(int *array, size_t size)
 {
         size_t i, j, temp;
 
-	if (size >= 2)
+	if (!array || size >= 2)
+                return ;
+	for (i = 0; i < (size - 1); i++)
 	{
-		for (i = 0; i < (size - 1); i++)
+		for (j = 0; j < (size - 1 - i); j++)
 		{
-			for (j = 0; j < (size - 1 - i); j++)
+			temp = 0;
+			if (array[j] > array[j + 1])
 			{
-				temp = 0;
-				if (array[j] > array[j + 1])
-				{
-					swap(&array[j + 1], &array[j]);
-					print_array(array, size);
-				}
+				swap(&array[j + 1], &array[j]);
+				print_array(array, size);
 			}
 		}
-	}
+	}}
 }
---
+++ 2-selection_sort.c
@@ -0,0 +1,43 @@
+#include"sort.h"
+
+/**
+ * swap - swaps intergers
+ * @x: first integer
+ * @y: second integer
+ */
+
+void swap(int *x, int *y)
+{
+        int temp = *x;
+        *x = *y;
+        *y = temp;
+}
+
+/**
+ * selection_sort - sorts the array in accending order
+ * @array: array to be sorted
+ * @size: size of array
+ */
+
+void selection_sort(int *array, size_t size)
+{
+        size_t i, j, min_value;
+        min_value = i;
+        if (!array || size < 2)
+                return;
+        for(i = 0; i < (size - 1); i++)
+        {
+                for (j = i + 1; j < size; j++)
+                {
+                        if (array[j] < array[min_value])
+                                array[j] = array[min_value];
+                }
+        }
+        if (min_value != i)
+        {
+                swap(&array[i], &array[min_value]);
+                print_array(array, size)
+        }
+}
+
+
