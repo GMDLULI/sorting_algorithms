6a3c1c8b20d226825e9fa2022a5a1972daf29eb6
GMDLULI <gugulethumdluli98@gmail.com>
2023-02-20 06:14:07 +0000

    bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -27,9 +27,9 @@ void bubble_sort(int *array, size_t size)
 
 	if (size >= 2)
 	{
-		for (i = 0; i < size; i++)
+		for (i = 0; i < (size - 1); i++)
 		{
-			for (j = 0; j < size - 1 - i; j++)
+			for (j = 0; j < (size - 1 - i); j++)
 			{
 				temp = 0;
 				if (array[j] > array[j + 1])
