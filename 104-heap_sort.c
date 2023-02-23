7d0ae74a735e145486b5fbe2632e257902c577ac
GMDLULI <gugulethumdluli98@gmail.com>
2023-02-23 03:06:47 +0000

    heap sort

--- 104-heap_sort.c
+++ 104-heap_sort.c
@@ -1,5 +1,9 @@
 #include "sort.h"
 
+void swap_ints(int *x, int *y);
+void heapify(int *array, size_t size, size_t base, size_t root);
+void heap_sort(int *array, size_t size);
+
 /**
  * swap_ints - swaps integers
  * @x: first integer to be swapped
@@ -9,42 +13,62 @@
 void swap_ints(int *x, int *y)
 {
 	int temp;
-	
+
 	temp = *x;
 	*x = *y;
 	*y = temp;
 }
 
-void heapify(int *array, size_t size, size_t base)
+/**
+ * heapify - Turn a binary tree into a complete binary heap.
+ * @array: An array of integers representing a binary tree.
+ * @size: The size of the array/tree.
+ * @base: The index of the base row of the tree.
+ * @root: The root node of the binary tree.
+ */
+void heapify(int *array, size_t size, size_t base, size_t root)
 {
-	int large = base;
-	int right = 2 * base + 1;
-	int left = 2 * base + 2;
+	int large = root;
+	int right = 2 * root + 1;
+	int left = 2 * root + 2;
 
-	if (left < size && array + left > array + large)
-		largest = left;
-	if (right < size && array + right > array + large)
-		largest = right;
+	if (left < base && array[left] > array[large])
+		large = left;
+	if (right < base && array[right] > array[large])
+		large = right;
 
-	if (large != base)
+	if (large != root)
 	{
-		swap(&array[base], &array[large]);
+		swap_ints(array + root, array + large);
 		print_array(array, size);
-		heapify(array, size, large);
+		heapify(array, size, base, large);
 	}
 }
+
+/**
+ * heap_sort - Sort an array of integers in ascending
+ *		order using heap sort algorithm
+ * @array: a pointer to the array of integers
+ * @size: the size of the array
+ *
+ * Description: implements the sift-down heap sort
+ */
 
 void heap_sort(int *array, size_t size)
 {
 	int i;
-	
+
+	if (array == NULL || size < 2)
+		return;
+
 	for (i = size / 2 - 1; i >= 0; i--)
-		heapify(array, i, 0);
+		heapify(array, size, size, i);
 
 	for (i = size - 1; i >= 0; i--)
 	{
 		swap_ints(&array[0], &array[i]);
-		heapify(array, i, 0);
+		print_array(array, size);
+		heapify(array, size, i, 0);
 	}
 }
 
