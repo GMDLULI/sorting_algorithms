523d3cb158790b8746ccaa0b091e16921be43fc0
KingHorey <oreoluwaakinbo.oa@gmail.com>
2023-02-23 02:36:24 +0100

    reversed

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,43 +1,29 @@
 #include "sort.h"
 
 /**
- * swap - swaps integers
- * @x: first integer
- * @y: second integer
- */
-
-void swap(int *x, int *y)
-{
-	int temp;
-
-	temp = *x;
-	*x = *y;
-	*y = temp;
-}
-
-/**
  * bubble_sort - Bubble sort algorithm for sorting items
  *
  * @array: pointer to the array that is to be sorted
  * @size: size of the array
  *
  */
-
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, j, len = size;
+	size_t i, j, temp;
 
-	if (array == NULL || size < 2)
-		return;
-
-	for (i = 0; i < len - 1; i++)
+	if (size >= 2)
 	{
-		for (j = 0; j < (len - 1 - i); j++)
+		for (i = 0; i < (size - 1); i++)
 		{
-			if (array[j] > array[j + 1])
+			for (j = 0; j < (size - 1 - i); j++)
 			{
-				swap(array + i, array + i - 1);
-				print_array(array, size);
+				if (array[j] > array[j + 1])
+				{
+					temp = array[j + 1];
+					array[j + 1] = array[j];
+					array[j] = temp;
+					print_array(array, size);
+				}
 			}
 		}
 	}
