c81d9a7b80b90d228be288693c53cf7ac043da96
GMDLULI <gugulethumdluli98@gmail.com>
2023-02-26 04:18:07 +0000

    merge

--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -1,4 +1,5 @@
 #include "sort.h"
+
 void merge_subarr(int *subarr, int *buff, size_t front, size_t mid,
 			size_t back);
 void merge_sort_recursive(int *subarr, int *buff, size_t front, size_t back);
@@ -6,34 +7,34 @@ void merge_sort(int *array, size_t size);
 
 /**
  * merge_subarr - Sort a subarray of integers.
- * @subarray: A subarray of an array of integers to sort.
+ * @subarr: A subarray of an array of integers to sort.
  * @buff: A buffer to store the sorted subarray.
  * @front: The front index of the array.
  * @mid: The middle index of the array.
  * @back: The back index of the array.
  */
-void merge_subarr(int *subarray, int *buff, size_t front,
+void merge_subarr(int *subarr, int *buff, size_t front,
 		size_t back, size_t mid)
 {
 	size_t i, j, k  = 0;
 
 	printf("Merging...\n[left]: ");
-	print_array(subarray + front, mid - front);
+	print_array(subarr + front, mid - front);
 
 	printf("[right]: ");
-	print_array(subarray + mid, back - mid);
+	print_array(subarr + mid, back - mid);
 
 	for (i = front, j = mid; i < mid && j < back; k++)
-		buff[k] = (subarray[i] < subarray[j]) ? subarray[i++] : subarray[j++];
+		buff[k] = (subarr[i] < subarr[j]) ? subarr[i++] : subarr[j++];
 	for (; i < mid; i++)
-		buff[k++] = subarray[i];
+		buff[k++] = subarr[i];
 	for (; j < back; j++)
-		buff[k++] = subarray[j];
+		buff[k++] = subarr[j];
 	for (i = front, k = 0; i < back; i++)
-		subarray[i] = buff[k++];
+		subarr[i] = buff[k++];
 
 	printf("[Done]: ");
-	print_array(subarray + front, back - front);
+	print_array(subarr + front, back - front);
 }
 
 /**
