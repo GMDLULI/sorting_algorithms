9f17905d9ae19c59561aa25ac8441bb097525d3e
GMDLULI <gugulethumdluli98@gmail.com>
2023-02-26 03:18:27 +0000

    bubble

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,7 +1,6 @@
 #include "sort.h"
 
 /**
-<<<<<<< HEAD
  * swap_ints - Swap two integers in an array.
  * @a: The first integer to swap.
  * @b: The second integer to swap.
@@ -19,18 +18,11 @@ void swap_ints(int *a, int *b)
  * bubble_sort - Sort an array of integers in ascending order.
  * @array: An array of integers to sort.
  * @size: The size of the array.
-=======
- * bubble_sort - Bubble sort algorithm for sorting items
- *
- * @array: pointer to the array that is to be sorted
- * @size: size of the array
->>>>>>> 8c66a1a1b7916dd01f244466ed20962321ad9c8e
  *
  * Description: Prints the array after each swap.
  */
 void bubble_sort(int *array, size_t size)
 {
-<<<<<<< HEAD
 	size_t i, len = size;
 	bool bubbly = false;
 
@@ -47,23 +39,6 @@ void bubble_sort(int *array, size_t size)
 				swap_ints(array + i, array + i + 1);
 				print_array(array, size);
 				bubbly = false;
-=======
-	size_t i, j, temp;
-
-	if (size >= 2)
-	{
-		for (i = 0; i < (size - 1); i++)
-		{
-			for (j = 0; j < (size - 1 - i); j++)
-			{
-				if (array[j] > array[j + 1])
-				{
-					temp = array[j + 1];
-					array[j + 1] = array[j];
-					array[j] = temp;
-					print_array(array, size);
-				}
->>>>>>> 8c66a1a1b7916dd01f244466ed20962321ad9c8e
 			}
 		}
 		len--;
